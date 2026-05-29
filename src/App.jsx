2ae8c51dc6bfe9817bc4b970ae1d71c1c4252401
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-05-29 18:02:36 +0530

    working on Industries component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Industries from './components/Industries'
 
 const App = () => {
   return (
     <div>
-<Navbar/>
-      <Hero/>
+      {/* <Navbar/>
+      <Hero/> */}
+      <Industries/>
 
     </div>
   )
---
+++ src/components/Industries.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import corporateImg from "../assets/industries/corporate.webp";
+import schoolImg from "../assets/industries/school.webp";
+import hospitalImg from "../assets/industries/hospital.webp";
+import hotelImg from "../assets/industries/hotel.webp";
+import factoryImg from "../assets/industries/factory.webp";
+
+const Industries = () => {
+  return (
+    <div className="max-w-[90rem] [font-family:'Mozilla_Headline',sans-serif]   mx-auto">
+      <div className="flex flex-col gap-30">
+        <div className="gap-3 flex flex-col">
+          <h3 className="text-5xl font-black">
+            Built for{" "}
+            <span className="bg-black text-white px-3">Every Workplace</span> in
+            India
+          </h3>
+          <p className="text-xl">
+            Whether you're a startup or an enterprise, if you have visitors, we
+            have the solution.
+          </p>
+        </div>
+
+        <div className="grid grid-cols-5 grid-rows-1 gap-4 border">
+          <div className="w-fit   flex flex-col  bg-black border-[#00000057] border-2 py-4 px-3 gap-4">
+            <div className="  px-4 flex justify-center">
+              <img
+                className="h-full brightness-0 invert w-40 object-fit"
+                src={corporateImg}
+                alt=""
+              />
+            </div>
+            <div className=" flex text-white justify-center flex-col text-center ">
+              <h4 className="text-3xl font-extrabold">Corporate Offices</h4>
+              <p className="text-lg text-[#ffffffba] ">
+                Control access, impress clients, log every guest professionally.
+              </p>
+            </div>
+          </div>
+
+          <div className="w-fit  flex flex-col  bg-black border-[#00000057] border-2 py-4 px-3 gap-4">
+            <div className="  px-4 flex justify-center">
+              <img
+                className="h-full brightness-0 invert w-40 object-fit"
+                src={schoolImg}
+                alt=""
+              />
+            </div>
+            <div className=" flex text-white justify-center flex-col text-center ">
+              <h4 className="text-3xl font-extrabold">Schools & Colleges</h4>
+              <p className="text-lg text-[#ffffffba] ">
+                Know who enters your campus. Keep students safe, always.
+              </p>
+            </div>
+          </div>
+
+          <div className="w-fit   flex flex-col  bg-black border-[#00000057] border-2 py-4 px-3 gap-4">
+            <div className="  px-4 flex justify-center">
+              <img
+                className="h-full brightness-0 invert w-40 object-fit"
+                src={hospitalImg}
+                alt=""
+              />
+            </div>
+            <div className=" flex text-white justify-center flex-col text-center ">
+              <h4 className="text-3xl font-extrabold">Hospitals & Clinics</h4>
+              <p className="text-lg text-[#ffffffba] ">
+                Manage patient attendants, restrict ward access, ensure hygiene
+                protocols.
+              </p>
+            </div>
+          </div>
+
+          <div className="w-fit  justify-around flex flex-col  bg-black border-[#00000057] border-2 py-4 px-3 gap-4">
+            <div className="  px-4 flex justify-center">
+              <img
+                className="h-full brightness-0 invert  object-fit"
+                src={factoryImg}
+                alt=""
+              />
+            </div>
+            <div className=" flex gap-4 text-white justify-center flex-col text-center ">
+              <h4 className="text-3xl font-extrabold">
+                Factories & Warehouses
+              </h4>
+              <p className="text-lg text-[#ffffffba] ">
+                Vendor & contractor management with safety compliance
+                documentation.
+              </p>
+            </div>
+          </div>
+
+          <div className="w-fit justify-between flex flex-col  bg-black border-[#00000057] border-2 py-4 px-3 gap-4">
+            <div className="  px-4 flex mx-auto">
+              <img
+                className="h-full brightness-0 invert  object-fit"
+                src={hotelImg}
+                alt=""
+              />
+            </div>
+            <div className=" flex gap-2 text-white justify-center flex-col text-center ">
+              <h4 className="text-3xl font-extrabold ">
+                Hotels & Residential Societies
+              </h4>
+              <p className="text-lg text-[#ffffffba] ">
+                Manage guests, deliveries, and service staff with secure and
+                seamlessly.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Industries;
