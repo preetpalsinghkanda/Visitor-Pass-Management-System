163cd6424338940acc508ae9d6edc2bf9c6ed0e1
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-06-02 11:36:04 +0530

    added Visitor Settings page

--- src/components/Visitor/VisitorSettings.jsx
+++ src/components/Visitor/VisitorSettings.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 const VisitorSettings = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
     <div className="max-w-[90rem] mx-auto border justify-center flex-col flex items-start h-screen">
       <div className="max-w-[35rem] p-10 border mx-auto  flex flex-col gap-6">
-        
         <div>
           <h3 className="text-5xl font-extrabold">Settings</h3>
           <p className="text-lg font-medium">
@@ -21,7 +22,7 @@ const VisitorSettings = () => {
                 alt=""
               />
             </div>
-            <span className="text-sm">CHANGE PHOTO</span>
+            <span className="text-sm cursor-pointer">CHANGE PHOTO</span>
           </div>
           <div>
             <h5 className="text-2xl font-bold">Profile Identity</h5>
@@ -55,20 +56,43 @@ const VisitorSettings = () => {
                 Role :<span className="text-md font-medium">VISITOR</span>
               </div>
             </div>
-
-            
           </div>
-         
         </div>
-         <div className=" flex justify-around">
-              <button className="px-6 py-2 border">Discard Changes</button>
-              <button className="px-6 py-2 border bg-black text-white">Save Changes</button>
-            </div>
-
+        <div className=" flex justify-around">
+          <button className="px-6 py-2 border">Discard Changes</button>
+          <button className="px-6 py-2 border bg-black text-white">
+            Save Changes
+          </button>
+        </div>
       </div>
 
-      <div className="max-w-[35rem] border mx-auto">
-        <h4>Preferences</h4>
+      <div className="max-w-[32.5rem] py-6 border mx-auto w-full my-5 px-10">
+        <h4 className=" text-xl font-bold">Preferences</h4>
+
+        <div className="my-3 flex justify-between ">
+          <div>
+            <h5 className="text-md">Visit Notifications</h5>
+            <p className="text-sm text-[#000000b5]">
+              Get notified when your visit is approved or rejected
+            </p>
+          </div>
+          <div class="relative inline-block w-11 h-5">
+            <input
+              checked
+              id="switch-component"
+              type="checkbox"
+              checked={isChecked}
+              onClick={() => {
+                setIsChecked(!isChecked);
+              }}
+              class="peer appearance-none   w-11 h-5 bg-slate-100 rounded-full checked:bg-black cursor-pointer transition-colors duration-300"
+            />
+            <label
+              for="switch-component"
+              class={`absolute top-0 left-0 w-5 h-5 ${isChecked ? "bg-white" : "bg-black"}  rounded-full border border-slate-300 shadow-sm transition-transform duration-300 peer-checked:translate-x-6 peer-checked:border-slate-800 cursor-pointer`}
+            ></label>
+          </div>
+        </div>
       </div>
     </div>
   );
