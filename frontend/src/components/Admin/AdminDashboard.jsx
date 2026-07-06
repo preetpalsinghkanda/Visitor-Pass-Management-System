ed527cfa20c632ed0913a9e3af1afe664c282d31
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-07-06 18:11:29 +0530

    added graph from recharts

--- frontend/src/components/Admin/AdminDashboard.jsx
+++ frontend/src/components/Admin/AdminDashboard.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
 
 const AdminDashboard = () => {
+  const sampleData = [
+    { month: "JAN", visits: 40 },
+    { month: "FEB", visits: 63 },
+    { month: "MAR", visits: 55 },
+    { month: "APR", visits: 81 },
+    { month: "MAY", visits: 95 },
+    { month: "JUN", visits: 72 },
+  ];
+
   return (
     <div className="max-w-[90rem]  mx-auto flex  px-4 py-4">
       <div className=" flex flex-col justify-center  w-fit px-10 ">
@@ -24,34 +41,53 @@ const AdminDashboard = () => {
         </ul>
       </div>
 
-      <div className="border flex items-start w-full">
-        <div className="border flex flex-col gap-4 flex-1 w-fit px-6 py-2">
-          <span className="flex gap-20 text-7xl font-extrabold justify-between">
-            20
-            <span class="material-symbols-outlined">call_made</span>
-          </span>
-          <p className="text-lg font-bold text-end">ALL VISITS</p>
+      <div className=" flex flex-col items-start w-full">
+        <div className="flex w-full">
+          <div className="border flex flex-col gap-4 flex-1 w-fit px-6 py-2">
+            <span className="flex gap-20 text-7xl font-extrabold justify-between">
+              20
+              <span class="material-symbols-outlined">call_made</span>
+            </span>
+            <p className="text-lg font-bold text-end">ALL VISITS</p>
+          </div>
+          <div className="border flex flex-col gap-4 flex-1 w-fit px-6 py-2">
+            <span className="flex gap-20 text-7xl font-extrabold justify-between">
+              20
+              <span class="material-symbols-outlined">call_made</span>
+            </span>
+            <p className="text-lg font-bold text-end">ALL VISITS</p>
+          </div>
+          <div className="border flex flex-col gap-4 flex-1 w-fit px-6 py-2">
+            <span className="flex gap-20 text-7xl font-extrabold justify-between">
+              20
+              <span class="material-symbols-outlined">call_made</span>
+            </span>
+            <p className="text-lg font-bold text-end">ALL VISITS</p>
+          </div>
+          <div className="border flex-1 w-fit gap-4 flex flex-col px-6 py-2">
+            <span className="flex gap-20 text-7xl font-extrabold justify-between">
+              20
+              <span class="material-symbols-outlined">call_made</span>
+            </span>
+            <p className="text-lg font-bold text-end">ALL VISITS</p>
+          </div>
         </div>
-        <div className="border flex flex-col gap-4 flex-1 w-fit px-6 py-2">
-          <span className="flex gap-20 text-7xl font-extrabold justify-between">
-            20
-            <span class="material-symbols-outlined">call_made</span>
-          </span>
-          <p className="text-lg font-bold text-end">ALL VISITS</p>
-        </div>
-        <div className="border flex flex-col gap-4 flex-1 w-fit px-6 py-2">
-          <span className="flex gap-20 text-7xl font-extrabold justify-between">
-            20
-            <span class="material-symbols-outlined">call_made</span>
-          </span>
-          <p className="text-lg font-bold text-end">ALL VISITS</p>
-        </div>
-        <div className="border flex-1 w-fit gap-4 flex flex-col px-6 py-2">
-          <span className="flex gap-20 text-7xl font-extrabold justify-between">
-            20
-            <span class="material-symbols-outlined">call_made</span>
-          </span>
-          <p className="text-lg font-bold text-end">ALL VISITS</p>
+
+        <div className="w-full border  p-6 shadow mt-8">
+          <h2 className="text-2xl font-bold mb-4">MONTHLY VISITS</h2>
+
+          <LineChart width={1000} height={500} data={sampleData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Line
+              type="monotone"
+              dataKey="visits"
+              stroke="#000"
+              strokeWidth={3}
+            />
+          </LineChart>
         </div>
       </div>
     </div>
