ee8005d361fd6c028aa106abbb6243f359e4b76c
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-07-15 20:21:14 +0530

    vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const employeeRoutes = require('./routes/employeeRoutes');
 const securityRoutes = require("./routes/securityRoutes");
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://visitor-pass-management-system-eb68.vercel.app"],
     credentials: true,
 }))
 app.use(express.json())
@@ -36,10 +36,13 @@ app.use('/employee', employeeRoutes)
 app.use('/security', securityRoutes)
 
 
-
-
-
-app.listen(PORT, () => {
-    console.log(`server running on ${PORT}`)
+app.get("/", (req, res) => {
+    res.json({ message: "backend is live" })
 })
 
+
+// app.listen(PORT, () => {
+//     console.log(`server running on ${PORT}`)
+// })
+
+module.exports = app
